bf105a8ba7a75123da3a99bfa9d3b0b37b1c1634
shrijo <jonas.shriqui@gmail.com>
2025-06-14 21:01:26 +0200

    new

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ReceiverPage from "./pages/Receiver";
-import SenderPage from "./pages/Sender";
+import Receiver from "./pages/Receiver";
+import Sender from "./pages/Sender";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/receiver" element={<ReceiverPage />} />
-        <Route path="/sender/:userId" element={<SenderPage />} />
+        <Route path="/" element={<Home />} /> {/* 👈 Add this */}
+        <Route path="/receiver" element={<Receiver />} />
+        <Route path="/sender/:userId" element={<Sender />} />
         <Route path="*" element={<h1>404 - Page not found</h1>} />
       </Routes>
     </Router>
+  );
+}
+
+// Simple home component
+function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Welcome to Pokedek 👾</h1>
+      <p>
+        Go to <a href="/receiver">/receiver</a> to start an experience.
+      </p>
+    </div>
   );
 }
